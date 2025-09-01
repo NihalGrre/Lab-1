5f1ee5d6c647bbb333a47c7ce7a60f7f25598d1d
NihalGre <65260212+NihalGrre@users.noreply.github.com>
2025-08-31 23:22:08 -0600

    Add files via upload

---
+++ L1q1NG.py
@@ -0,0 +1,39 @@
+#------------------
+#Name: Nihal Grewal
+#Program: L1Q1NG.py
+#-------------------
+#Purpose: To learn some programming tips
+#-------------------
+
+print("\nhere are some programming tips:")
+
+#print a blank line
+print()
+print('1) print a blank line with: print()')
+print('      or use the new line \\n (backslash n)symbol\n')
+
+#use commas to seperate data in a print statement
+print('2) Sepereate data', 'in a', 'print statement', 'with commas')
+print('       Each comma seperator','will add','one space','to the output.\n')
+
+#use sep to change the default seperator action in a print statement
+print('3) change the', 'default','seperator','action using sep', sep = '---')
+print()
+
+#use end to change the defualt line termination action in a print statement
+print('4) combine print statement outputs', end = " ")
+print('(on the same line)', end = " ")
+print('with end\n')
+
+#concatenate text strings using +
+print('5) Concatenate' + 'strings' + 'using' + '+' + 'to' + 'prevent' + 'spaces\n')
+
+#tab using \t
+print('6) Tab', '\t', 'the text', '\twith the \\t(backslash t) symbol\n')
+
+#store information in variables
+print("7) use a variable to store information:")
+print('\tSet variable x equal to (6-7)')
+x = (6 - 7)
+print("\t3+(x*4)=", 3+(x*4))
+
